8b958e17ae67a2c393969ba1389324c9d418828c
Michael Frieze <michaelfrieze@icloud.com>
2023-07-23 08:31:39 -0400

    Installed prettier-plugin-tailwindcss and formatted all files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<h1 class="flex text-3xl font-bold underline">Hello world!</h1>
 
 <style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.300);
-	}
+  :global(html) {
+    background-color: theme(colors.gray.300);
+  }
 </style>
