d66fa95e936cead1e18bd37d646df3326f062c04
Michael Frieze <michaelfrieze@icloud.com>
2023-07-24 14:11:16 -0400

    Checking out how aliases work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
+<script>
+  import { formatMe } from '$utils/FormatText';
+</script>
+
 <h1 class="flex text-3xl font-bold underline">Hello world!</h1>
 
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.300);
-  }
-</style>
+<h3>{formatMe('Mike')}</h3>
